b0724dc5e7df11121187e563d641daa793c716d8
Eddie <e.otero81@gmail.com>
2019-01-03 23:00:27 -0500

    add post list component

--- mean/src/app/app.module.ts
+++ mean/src/app/app.module.ts
@@ -4,16 +4,25 @@ import { FormsModule } from "@angular/forms";
 import {
   MatInputModule,
   MatCardModule,
-  MatButtonModule
+  MatButtonModule,
+  MatToolbarModule,
+  MatExpansionModule
 } from "@angular/material";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { PostCreateComponent } from "./posts/post-create/post-create/post-create.component";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { HeaderComponent } from "./header/header.component";
+import { PostListComponent } from "./posts/post-list/post-list.component";
 
 @NgModule({
-  declarations: [AppComponent, PostCreateComponent],
+  declarations: [
+    AppComponent,
+    PostCreateComponent,
+    HeaderComponent,
+    PostListComponent
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
@@ -21,7 +30,9 @@ import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
     BrowserAnimationsModule,
     MatInputModule,
     MatCardModule,
-    MatButtonModule
+    MatButtonModule,
+    MatToolbarModule,
+    MatExpansionModule
   ],
   providers: [],
   bootstrap: [AppComponent]
